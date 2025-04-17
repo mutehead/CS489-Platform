a6d0c487131fa0e062e0be735f7d681559b939aa
Nishant Gurung <m.nishant.c@gmail.com>
2025-04-16 22:49:51 -0400

    Add files via upload

---
+++ maps/map2objects.lua
@@ -0,0 +1,61 @@
+local objects = {
+    {
+      id = 1,
+      name = "Coin",
+      type = "",
+      shape = "rectangle",
+      x = 100,
+      y = 260,
+      width = 16,
+      height = 16,
+      rotation = 0,
+      gid = 49,
+      visible = true,
+      properties = {}
+    },
+    {
+      id = 2,
+      name = "Gem",
+      type = "",
+      shape = "rectangle",
+      x = 460,
+      y = 250, 
+      width = 16,
+      height = 16,
+      rotation = 0,
+      gid = 54,
+      visible = true,
+      properties = {}
+    },
+    {
+      id = 3,
+      name = "Gem",
+      type = "",
+      shape = "rectangle",
+      x = 720,
+      y = 270,
+      width = 16,
+      height = 16,
+      rotation = 0,
+      gid = 54,
+      visible = true,
+      properties = {}
+    },
+    {
+      id = 4,
+      name = "Gem",
+      type = "",
+      shape = "rectangle",
+      x = 750,
+      y = 120,
+      width = 16,
+      height = 16,
+      rotation = 0,
+      gid = 54,
+      visible = true,
+      properties = {}
+    }
+  }
+
+
+return objects
